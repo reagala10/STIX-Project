017f1a3f044db479331a57798e3ecc8b78ab77c6
reagala10 <32242216+reagala10@users.noreply.github.com>
2022-03-23 23:13:00 -0400

    Added json file upload functionality

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import glob, os
+from wtforms.validators import InputRequired
+import json
+import re
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
 
-@app.route('/')
-def index():
-    #return "Hello World"
-    return render_template('index.html')
-    #return app
-#app = create_app()
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
 
-if __name__ == "__main__":
+@app.route('/', methods=['GET',"POST"])
+@app.route('/home', methods=['GET',"POST"])
+
+
+def home():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data # First grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        os.chdir("/Users/rohiteagala/python_projects/STIX/static/files")
+        return "File uploaded"
+    return render_template('index.html', form=form)
+
+if __name__ == '__main__':
     app.run(debug=True)
